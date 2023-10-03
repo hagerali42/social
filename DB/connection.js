4442734b0d10ed7ea6e8f6f93b834ec6f094aa9e
Hager Ali <hageraliali42@gmail.com>
2023-10-03 18:15:07 +0300

    c

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 const connectDB  = async ()=>{
     console.log(process.env.DB_URl);
-    return await mongoose.connect(process.env.DB_URl,{
+    return await mongoose.connect(`${process.env.DB_URl}`,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
