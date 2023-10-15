2c793957ab85393898137a0343a4a4450eaa873f
Hager Ali <hageraliali42@gmail.com>
2023-10-15 20:34:15 +0300

    commit "j"

--- index.js
+++ index.js
@@ -11,38 +11,44 @@ import { authSoket } from "./src/middleware/auth.js";
 import cors from "cors";
 import userModel from "./DB/model/User.model.js";
 const app = express();
+app.use(cors());
 const allowedOrigins = [
   "http://localhost:3000",
   "https://localhost:3000",
   "http://localhost:3001",
   "https://localhost:3001",
   "https://social-qftn.onrender.com",
-  "*",
 ];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Check if the origin is in the allowedOrigins array
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // Enable cookies and authentication
-  })
-);
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  // Other CORS headers and settings
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  next();
+});
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // Check if the origin is in the allowedOrigins array
+//       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true, // Enable cookies and authentication
+//   })
+// );
 
 
-
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Specify the methods you want to allow
-    credentials: true,
-    optionsSuccessStatus: 204, // Handle preflight requests
-  })
-);
 // setup port and the baseUrl
 const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
