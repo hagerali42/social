e588e4e0e05f53ea91e44512822f42e86b6631dc
Hager Ali <hageraliali42@gmail.com>
2023-10-15 14:23:35 +0300

    commit "f"

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ import cors from "cors";
 import userModel from "./DB/model/User.model.js";
 const app = express();
 app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  })
+)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS,GET,POST,PUT,PATCH,DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // setup port and the baseUrl
 const port = process.env.PORT || 5000;
--- src/utils/socketio.js
+++ src/utils/socketio.js
@@ -1,11 +1,15 @@
 import { Server } from "socket.io"
+import cors from 'cors'
 // import { Server } from "socket.io/dist/index.js";
 
 // const io = require("socket.io")(8800, {
 //     cors: {
 //       origin: "http://localhost:3000",
+//      methods: ["GET", "POST"],
 //     },
 //   });
+
+
 let io 
 export const initIo =(server)=>{
 
