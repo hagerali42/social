715f9cd079b74a96c24d75f97f6f411d1544c3d6
Hager Ali <hageraliali42@gmail.com>
2023-10-20 00:46:32 +0300

    commit "L"

--- src/modules/chat/chat.router.js
+++ src/modules/chat/chat.router.js
@@ -1,23 +1,29 @@
 import { Router } from "express";
 import * as chatController from './controller/chat.js'
 import { asyncHandler } from './../../utils/errorHandling.js';
+import * as Val from './chat.validation.js'
 import { auth } from "../../middleware/auth.js";
+import { validation } from "../../middleware/validation.js";
 const router =Router()
 
-router.route("/")
-    .post(
-        auth,
-        asyncHandler(chatController.accessChat)
-    )
-    .get(
-        auth,
-        asyncHandler(chatController.fetchChats)
-    );
+router
+  .route("/")
+  .post(
+    auth,
+    validation(Val.accessChat),
+    asyncHandler(chatController.accessChat)
+  )
+  .get(
+    auth,
+    validation(Val.fetchChats),
+    asyncHandler(chatController.fetchChats)
+  );
 
 
 router.route("/group")
     .post(
         auth,
+        validation(Val.createGroupChat),
         asyncHandler(chatController.createGroupChat)
     );
    
@@ -25,19 +31,24 @@ router.route("/group")
 router.route("/rename")
     .put(
         auth,
+        validation(Val.updateRoomName),
        asyncHandler( chatController.renameGroup)
     );
 
-router.route("/groupremove")
-    .put(
-        auth,
-        asyncHandler(chatController.removeFromGroup)
-    );
-router.route("/groupadd")
-    .put(
-        auth,
-        asyncHandler(chatController.addToGroup)
-    );
+router
+  .route("/groupremove")
+  .put(
+    auth,
+    validation(Val.addUsersToGroupOrDElet),
+    asyncHandler(chatController.removeFromGroup)
+  );
+router
+  .route("/groupadd")
+  .put(
+    auth,
+    validation(Val.addUsersToGroupOrDElet),
+    asyncHandler(chatController.addToGroup)
+  );
 
 
 
---
+++ src/modules/chat/chat.validation.js
@@ -0,0 +1,47 @@
+import joi from "joi";
+import { generalFields } from "../../middleware/validation.js";
+
+export const accessChat = {
+  body: joi
+    .object()
+    .required()
+    .keys({
+      userId: generalFields.id,
+      chatName: joi.string(),
+   
+    }),
+  params: joi.object().required().keys({}),
+  query: joi.object().required().keys({}),
+};
+
+export const fetchChats = {
+  body: joi.object().required().keys({
+  }),
+  params: joi.object().required().keys({}),
+  query: joi.object().required().keys({}),
+};
+export const createGroupChat = {
+  body: joi.object().required().keys({
+    name: joi.string().required(),
+    users:joi.array()
+  }),
+  params: joi.object().required().keys({}),
+  query: joi.object().required().keys({}),
+};
+
+export const updateRoomName = {
+  body: joi.object().required().keys({
+    chatId: generalFields.id,
+    chatName: joi.string().required(),
+  }),
+  params: joi.object().required().keys({}),
+  query: joi.object().required().keys({}),
+};
+export const addUsersToGroupOrDElet = {
+  body: joi.object().required().keys({
+    chatId: generalFields.id,
+    userId: generalFields.id,
+  }),
+  params: joi.object().required().keys({}),
+  query: joi.object().required().keys({}),
+};
