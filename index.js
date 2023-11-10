f65b4ca2bf16d865d91dba2d41a24cd755cd5442
Hager Ali <hageraliali42@gmail.com>
2023-11-10 14:15:06 +0200

    commit"D"

--- index.js
+++ index.js
@@ -15,21 +15,22 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "https://www.google.com",
-  "http://localhost:3000",
   "https://vocal-seahorse-a841c0.netlify.app",
 ];
+
 
 app.use(
   cors({
     origin: function (origin, callback) {
+       console.log("Request Origin:", origin);
+
       if (!origin) {
         // Requests with no origin (e.g., same-origin requests) are allowed
         return callback(null, true);
       }
 
       if (allowedOrigins.indexOf(origin) === -1) {
-        const message =
-          "The CORS policy for this site does not allow access from the specified origin.";
+        const message ="The CORS policy for this site does not allow access from the specified origin.";
         return callback(new Error(message), false);
       }
 
